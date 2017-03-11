9598cb6a8fe6ce31f29c6b1dec26001c76ad08fd
Matheus Mafra <matheuslpm@gmail.com>
2017-03-11 12:33:41 -0300

    Inciando o evento de 2017

--- docpad.js
+++ docpad.js
@@ -1,198 +1,193 @@
 module.exports = {
 
-  prompts: false,
+    prompts: false,
 
-  // These are variables will be accessible via our templates
-  templateData: {
+    // These are variables will be accessible via our templates
+    templateData: {
 
-    // Conference info
-    conf: {
-      name: "Unidev",
-      description: "3ª edição",
-      date: "04 de Junho",
-      // If your event is free, just comment this line
-      //price: "$100",
-      venue: "Unifor",
-      address: "Av. Washington Soares, 1321 - Edson Queiroz, Fortaleza - CE, 60811-905",
-      city: "Fortaleza",
-      state: "Ceará"
-    },
-
-    // The Call To Action button at the header,
-    // If you don't want this, just remove the callToAction property.
-    callToAction: {
-        text: "Inscrição",
-        link: "http://eventick.com.br"
-    },
-
-    // "Fork me on GitHub", if you don't want this, just remove the forkButton property
-    forkButton: {
-        repository: "https://github.com/UnidevCE/unidevce.github.io"
-    },
-
-    // Site info
-    site: {
-      theme: "yellow-swan",
-      url: "http://braziljs.github.io/conf-boilerplate/",
-      googleanalytics: "UA-33656081-1"
-    },
-
-    // Active sections on the website
-    // to deactivate comment out with '//'
-    // you can also change order here and it will reflect on page
-    sections: [
-      'about',
-      'location',
-      'speakers',
-      'schedule',
-      //'sponsors',
-      'partners'
-      // 'contact'
-    ],
-
-    // Labels which you can translate to other languages
-    labels: {
-      about: "Sobre",
-      location: "Localização",
-      speakers: "Palestrantes",
-      schedule: "Agenda",
-      sponsors: "Patrocinadores",
-      partners: "Parceiros",
-      contact: "Contact"
-    },
-
-    // The entire schedule
-    schedule: [
-      {
-        name: "Check-in / Breakfast",
-        time: "9h00"
-      },
-      {
-        name: "Linus Torvalds",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Linux Foundation",
-        link: {
-          href: "http://twitter.com/linus",
-          text: "@linus"
+        // Conference info
+        conf: {
+            name: "Unidev 1.0",
+            description: "Evento de desenvolvimento. 1ª edição",
+            date: "06 de Maio",
+            // If your event is free, just comment this line
+            //price: "$100",
+            venue: "Unifor",
+            address: "Av. Washington Soares, 1321 - Edson Queiroz, Fortaleza - CE, 60811-905",
+            city: "Fortaleza",
+            state: "Ceará"
         },
-        presentation: {
-          title: "Digging into a Linux Kernel",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "10h00"
-        }
-      },
-      {
-        name: "Bill Gates",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Microsoft",
-        link: {
-          href: "http://github.com/billy95",
-          text: "@billy95"
-        },
-        presentation: {
-          title: "Introducing Windows 12",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "11h00"
-        }
-      },
-      {
-        name: "Lunch",
-        time: "12h00"
-      },
-      {
-        name: "Chuck Norris",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Delta Command",
-        link: {
-          href: "http://twitter.com/littlechuck",
-          text: "@littlechuck"
-        },
-        presentation: {
-          title: "How to kill a elephant with one finger",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "13h00"
-        }
-      },
-      {
-        name: "Steve Jobs",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Apple, Inc.",
-        link: {
-          href: "http://github.com/stevie",
-          text: "@stevie"
-        },
-        presentation: {
-          title: "Presenting iPad",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "14h00"
-        }
-      },
-      {
-        name: "Coffee-break",
-        time: "15h00"
-      },
-      {
-        name: "Mark Zuckerberg",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Facebook",
-        link: {
-          href: "http://twitter.com/zuck",
-          text: "@zuck"
-        },
-        presentation: {
-          title: "Revealing Facebook Secrets",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "16h00"
-        }
-      },
-      {
-        name: "Steve Wozniak",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Apple, Inc.",
-        link: {
-          href: "http://twitter.com/woz",
-          text: "@woz"
-        },
-        presentation: {
-          title: "Why do I prefer Android over iPhone",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "17h00"
-        }
-      }
-    ],
 
-    // List of Sponsors
-    sponsors: [
-      {
-        name: "Eventick",
-        logo: "themes/yellow-swan/img/sponsor.png",
-        url: "http://eventick.com.br"
-      }
-    ],
+        // The Call To Action button at the header,
+        // If you don't want this, just remove the callToAction property.
+        callToAction: {
+            text: "Submeta sua palesta!",
+            link: "http://eventick.com.br"
+        },
 
-    // List of Partners
-    partners: [
-      {
-        name: "Unifor",
-        logo: "http://www.jaoodontounifor.com/wp-content/uploads/2015/09/uniforlogo.png",
-        url: "http://www.unifor.br/"
-      }
-    ],
+        // "Fork me on GitHub", if you don't want this, just remove the forkButton property
+        forkButton: {
+            repository: "https://github.com/UnidevCE/unidevce.github.io"
+        },
 
-    // Theme path
-    getTheme: function() {
-      return "themes/" + this.site.theme;
-    },
+        // Site info
+        site: {
+            theme: "yellow-swan",
+            url: "http://braziljs.github.io/conf-boilerplate/",
+            googleanalytics: "UA-33656081-1"
+        },
 
-    // Site Path
-    getUrl: function() {
-    	return this.site.url;
+        // Active sections on the website
+        // to deactivate comment out with '//'
+        // you can also change order here and it will reflect on page
+        sections: [
+            'about',
+            'location',
+            // 'speakers',
+            // 'schedule',
+            //'sponsors',
+            // 'partners'
+            // 'contact'
+        ],
+
+        // Labels which you can translate to other languages
+        labels: {
+            about: "Sobre",
+            location: "Localização",
+            speakers: "Palestrantes",
+            schedule: "Agenda",
+            sponsors: "Patrocinadores",
+            partners: "Parceiros",
+            contact: "Contact"
+        },
+
+        // The entire schedule
+        schedule: [{
+                name: "Check-in / Breakfast",
+                time: "9h00"
+            },
+            {
+                name: "Linus Torvalds",
+                photo: "themes/yellow-swan/img/speaker.jpg",
+                bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                company: "Linux Foundation",
+                link: {
+                    href: "http://twitter.com/linus",
+                    text: "@linus"
+                },
+                presentation: {
+                    title: "Digging into a Linux Kernel",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                    time: "10h00"
+                }
+            },
+            {
+                name: "Bill Gates",
+                photo: "themes/yellow-swan/img/speaker.jpg",
+                bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                company: "Microsoft",
+                link: {
+                    href: "http://github.com/billy95",
+                    text: "@billy95"
+                },
+                presentation: {
+                    title: "Introducing Windows 12",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                    time: "11h00"
+                }
+            },
+            {
+                name: "Lunch",
+                time: "12h00"
+            },
+            {
+                name: "Chuck Norris",
+                photo: "themes/yellow-swan/img/speaker.jpg",
+                bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                company: "Delta Command",
+                link: {
+                    href: "http://twitter.com/littlechuck",
+                    text: "@littlechuck"
+                },
+                presentation: {
+                    title: "How to kill a elephant with one finger",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                    time: "13h00"
+                }
+            },
+            {
+                name: "Steve Jobs",
+                photo: "themes/yellow-swan/img/speaker.jpg",
+                bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                company: "Apple, Inc.",
+                link: {
+                    href: "http://github.com/stevie",
+                    text: "@stevie"
+                },
+                presentation: {
+                    title: "Presenting iPad",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                    time: "14h00"
+                }
+            },
+            {
+                name: "Coffee-break",
+                time: "15h00"
+            },
+            {
+                name: "Mark Zuckerberg",
+                photo: "themes/yellow-swan/img/speaker.jpg",
+                bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                company: "Facebook",
+                link: {
+                    href: "http://twitter.com/zuck",
+                    text: "@zuck"
+                },
+                presentation: {
+                    title: "Revealing Facebook Secrets",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                    time: "16h00"
+                }
+            },
+            {
+                name: "Steve Wozniak",
+                photo: "themes/yellow-swan/img/speaker.jpg",
+                bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                company: "Apple, Inc.",
+                link: {
+                    href: "http://twitter.com/woz",
+                    text: "@woz"
+                },
+                presentation: {
+                    title: "Why do I prefer Android over iPhone",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+                    time: "17h00"
+                }
+            }
+        ],
+
+        // List of Sponsors
+        sponsors: [{
+            name: "Eventick",
+            logo: "themes/yellow-swan/img/sponsor.png",
+            url: "http://eventick.com.br"
+        }],
+
+        // List of Partners
+        partners: [{
+            name: "Unifor",
+            logo: "http://www.jaoodontounifor.com/wp-content/uploads/2015/09/uniforlogo.png",
+            url: "http://www.unifor.br/"
+        }],
+
+        // Theme path
+        getTheme: function() {
+            return "themes/" + this.site.theme;
+        },
+
+        // Site Path
+        getUrl: function() {
+            return this.site.url;
+        }
     }
-  }
 };
